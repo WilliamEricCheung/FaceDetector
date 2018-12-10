2e91235b39b740ee9f2666661f3722c4ba0a9341
William Eric Cheung <williamericcheung@outlook.com>
2018-12-10 14:17:02 +0800

    fix

--- app/src/main/java/tech/wec/pythonndkdemo/MainActivity.java
+++ app/src/main/java/tech/wec/pythonndkdemo/MainActivity.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void loadPy(String appLib){
-
-        // Extract python files from assets
+        //拷贝Python相关环境
+        //Extract python files from assets
         AssetExtractor assetExtractor = new AssetExtractor(this);
         assetExtractor.removeAssets("python");
         assetExtractor.copyAssets("python");
@@ -77,14 +78,13 @@ public class MainActivity extends AppCompatActivity {
         StarObjectClass pythonSys = python._GetObject("sys");
         StarObjectClass pythonPath = (StarObjectClass) pythonSys._Get("path");
         pythonPath._Call("insert", 0, appFile.getPath()+ File.separator +"python3.4.zip");
-        pythonPath._Call("insert", 0, appFile.getPath()+ File.separator +"cv2");
         pythonPath._Call("insert", 0, appLib);
         pythonPath._Call("insert", 0, appFile.getPath());
 
         //调用Python代码
-        Service._DoFile("python", appFile.getPath() + "/py_code.zip", "");
+        Service._DoFile("python", appFile.getPath() + "/python/py_code.py", "");
         long time = python._Calllong("get_time");
-        Log.d("", "from python time="+time);
+        Log.d("", "form python time="+time);
 
         Service._DoFile("python", appFile.getPath() + "/test.py", "");
         int result = python._Callint("add", 5, 2);
@@ -93,11 +93,10 @@ public class MainActivity extends AppCompatActivity {
         python._Set("JavaClass", Log.class);
         Service._DoFile("python", appFile.getPath() + "/calljava.py", "");
 
-        Service._DoFile("python", appFile.getPath() + "/cv_test.py","");
-        Service._DoFile("python", appFile.getPath() + "/test.py","");
-        Object object = python._Call("hello");
-        if(object !=null){
-            Log.d("",object.toString());
+        List<String> assetList = assetExtractor.listAssets("");
+        for (String asset: assetList
+                ) {
+            Log.i("Asset List:", asset);
         }
     }
 
@@ -126,4 +125,112 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+//    public StarSrvGroupClass SrvGroup;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        initPy();
+//    }
+//
+//    protected void initPy(){
+//
+//        final String appLib = getApplicationInfo().nativeLibraryDir;
+//
+//        AsyncTask.execute(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                loadPy(appLib);
+//            }
+//        });
+//    }
+//
+//    void loadPy(String appLib){
+//        //拷贝Python相关环境
+//        //Extract python files from assets
+//        AssetExtractor assetExtractor = new AssetExtractor(this);
+//        assetExtractor.removeAssets("python");
+//        assetExtractor.copyAssets("python");
+//
+//        final File appFile = getFilesDir();  /*-- /data/data/packageName/files --*/
+//
+//        try {
+//            // 加载Python解释器
+//            System.load(appLib + File.separator + "libpython3.4m.so");
+//
+//            // 除了将代码直接拷贝，还支持将代码压缩为zip包，通过Install方法解压到指定路径
+//            InputStream dataSource = getAssets().open("py_code.zip");
+//            StarCoreFactoryPath.Install(dataSource, appFile.getPath(),true );
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        /*----init starcore----*/
+//        StarCoreFactoryPath.StarCoreCoreLibraryPath = appLib;
+//        StarCoreFactoryPath.StarCoreShareLibraryPath = appLib;
+//        StarCoreFactoryPath.StarCoreOperationPath = appFile.getPath();
+//
+//        StarCoreFactory starcore = StarCoreFactory.GetFactory();
+//        StarServiceClass Service = starcore._InitSimple("test", "123", 0, 0);
+//        SrvGroup = (StarSrvGroupClass) Service._Get("_ServiceGroup");
+//        Service._CheckPassword(false);
+//
+//        /*----run python code----*/
+//        SrvGroup._InitRaw("python34", Service);
+//        StarObjectClass python = Service._ImportRawContext("python", "", false, "");
+//        // 设置Python模块加载路径
+//        python._Call("import", "sys");
+//        StarObjectClass pythonSys = python._GetObject("sys");
+//        StarObjectClass pythonPath = (StarObjectClass) pythonSys._Get("path");
+//        pythonPath._Call("insert", 0, appFile.getPath()+ File.separator +"python3.4.zip");
+//        pythonPath._Call("insert", 0, appLib);
+//        pythonPath._Call("insert", 0, appFile.getPath());
+//
+//        //调用Python代码
+//        Service._DoFile("python", appFile.getPath() + "/python/py_code.py", "");
+//        long time = python._Calllong("get_time");
+//        Log.d("", "form python time="+time);
+//
+//        Service._DoFile("python", appFile.getPath() + "/test.py", "");
+//        int result = python._Callint("add", 5, 2);
+//        Log.d("", "result="+result);
+//
+//        python._Set("JavaClass", Log.class);
+//        Service._DoFile("python", appFile.getPath() + "/calljava.py", "");
+//
+//        List<String> assetList = assetExtractor.listAssets("");
+//        for (String asset: assetList
+//             ) {
+//            Log.i("Asset List:", asset);
+//        }
+//    }
+//
+//    private void copyFile(Context c, String Name) {
+//        File outfile = new File(c.getFilesDir(), Name);
+//        BufferedOutputStream outStream = null;
+//        BufferedInputStream inStream = null;
+//
+//        try {
+//            outStream = new BufferedOutputStream(new FileOutputStream(outfile));
+//            inStream = new BufferedInputStream(c.getAssets().open(Name));
+//
+//            byte[] buffer = new byte[1024 * 10];
+//            int readLen = 0;
+//            while ((readLen = inStream.read(buffer)) != -1) {
+//                outStream.write(buffer, 0, readLen);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (inStream != null) inStream.close();
+//                if (outStream != null) outStream.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
