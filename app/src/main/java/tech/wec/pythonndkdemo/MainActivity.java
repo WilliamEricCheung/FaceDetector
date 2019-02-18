961409bded3baf3f2bc3ee9673c2f349557e2705
William Eric Cheung <williamericcheung@outlook.com>
2019-02-18 20:18:07 +0800

    add some GUI

--- app/src/main/java/tech/wec/pythonndkdemo/MainActivity.java
+++ app/src/main/java/tech/wec/pythonndkdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package tech.wec.pythonndkdemo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +21,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final int REQUEST_CAMERA_PERMISSION = 0;
     protected Python python = Python.getInstance();
 
     @Override
@@ -27,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
         python.getModule("py_numpy").callAttr("numpyTest");
         python.getModule("py_opencv").callAttr("cv2Test");
         python.getModule("py_tensorflow").callAttr("tfTest");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("CameraNew", "Lacking privileges to access camera service, please request permission first.");
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, REQUEST_CAMERA_PERMISSION);
+        }
     }
 }
